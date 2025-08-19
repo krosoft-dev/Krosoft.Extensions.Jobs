2f0cb0f893f80081464e5194cbd15394edd8dd3b
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-19 20:49:25 +0200

    Ajout des projets de tests unitaires

---
+++ tests/Krosoft.Extensions.Jobs.Hangfire.Tests/Profiles/HangfireProfileTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Hangfire.Storage;
+using Krosoft.Extensions.Jobs.Hangfire.Models;
+using Krosoft.Extensions.Jobs.Hangfire.Profiles;
+
+namespace Krosoft.Extensions.Jobs.Hangfire.Tests.Profiles
+{
+    [TestClass]
+    public class HangfireProfileTests
+    {
+        private readonly IMapper _mapper;
+
+        public HangfireProfileTests()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<HangfireProfile>());
+            _mapper = config.CreateMapper();
+        }
+
+        [TestMethod]
+        public void Map_RecurringJobDto_To_CronJob_Should_Be_Valid()
+        {
+            // Arrange
+            var recurringJobDto = new RecurringJobDto
+            {
+                Id = "job1",
+                Cron = "0 0 * * *",
+                NextExecution = DateTime.UtcNow,
+                LastJobState = "Succeeded",
+                LastExecution = DateTime.UtcNow.AddDays(-1),
+                CreatedAt = DateTime.UtcNow.AddMonths(-1)
+            };
+
+            // Act
+            var cronJob = _mapper.Map<CronJob>(recurringJobDto);
+
+            // Assert
+            Check.That(cronJob.Identifiant).IsEqualTo(recurringJobDto.Id);
+            Check.That(cronJob.CronExpression).IsEqualTo(recurringJobDto.Cron);
+            Check.That(cronJob.ProchaineExecutionDate).IsEqualTo(recurringJobDto.NextExecution);
+            Check.That(cronJob.DerniereExecutionStatut).IsEqualTo(recurringJobDto.LastJobState);
+            Check.That(cronJob.DerniereExecutionDate).IsEqualTo(recurringJobDto.LastExecution);
+            Check.That(cronJob.CreationDate).IsEqualTo(recurringJobDto.CreatedAt);
+        }
+    }
+}
---
+++ tests/Krosoft.Extensions.Jobs.Tests/Services/FireForgetServiceTests.cs
@@ -0,0 +1,73 @@
+﻿using Krosoft.Extensions.Jobs.Extensions;
+using Krosoft.Extensions.Jobs.Interfaces;
+using Krosoft.Extensions.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Krosoft.Extensions.Jobs.Services.Tests
+{
+    [TestClass]
+    public class FireForgetServiceTests : BaseTest
+    {
+        protected override void AddServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddFireForget();
+        }
+
+        [TestMethod]
+        public void FireTest()
+        {
+            using var serviceProvider = CreateServiceCollection();
+
+            var service = serviceProvider.GetRequiredService<IFireForgetService>();
+
+            service.Fire<INotificationService>(null!);
+
+            //Check.That(service.Now.DateTime).IsEqualTo(new DateTime(2012, 1, 3));
+
+            //Assert.Fail();
+        }
+
+        [TestMethod]
+        public void FireAsyncTest()
+        {
+            var services = new ServiceCollection();
+            services.AddFireForget();
+            var buildServiceProvider = services.BuildServiceProvider();
+            var service = buildServiceProvider.GetRequiredService<IFireForgetService>();
+            service.FireAsync<INotificationService>(null!);
+
+            //Check.That(delta).IsLessThan(threshold);
+
+            //Assert.Fail();
+        }
+    }
+}
+
+public class INotificationService
+{
+//    private readonly IFireForgetService _fireForgetService;
+//    private readonly IPositiveTokenBuilderService _positiveTokenBuilderService;
+//    public INotificationService(IFireForgetService fireForgetService, IPositiveTokenBuilderService positiveTokenBuilderService)
+//    {
+//        _fireForgetService = fireForgetService;
+//        _positiveTokenBuilderService = positiveTokenBuilderService;
+//    }
+//    public void Publish(INotification notification, CancellationToken cancellationToken)
+//    {
+//        _fireForgetService.FireAsync<IMediator>(async mediator =>
+//        {
+//            var positiveToken = _positiveTokenBuilderService.Build();
+//            await mediator.Publish(notification, cancellationToken);
+//        });
+//    }
+
+//    public void Publish(Func<KrosoftToken, INotification> func, CancellationToken cancellationToken)
+//{
+//    _fireForgetService.FireAsync<IMediator>(async mediator =>
+//    {
+//        var positiveToken = _positiveTokenBuilderService.Build();
+
+//        await mediator.Publish(func(positiveToken), cancellationToken);
+//    });
+}
