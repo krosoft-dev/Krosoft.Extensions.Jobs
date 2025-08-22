09fa021ead5a850ac993c9c47e9ea82547c16caa
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-22 10:42:51 +0200

    update tests

--- tests/Krosoft.Extensions.Samples.DotNet9.Api.Tests/Functional/JobsEndpointTests.cs
+++ tests/Krosoft.Extensions.Samples.DotNet9.Api.Tests/Functional/JobsEndpointTests.cs
@@ -1,9 +1,10 @@
+using System.Net;
+using JetBrains.Annotations;
 using Krosoft.Extensions.Core.Extensions;
+using Krosoft.Extensions.Core.Models.Dto;
 using Krosoft.Extensions.Samples.DotNet9.Api.Features.Jobs;
 using Krosoft.Extensions.Samples.DotNet9.Api.Features.Jobs.Jobs;
 using Krosoft.Extensions.Samples.DotNet9.Api.Tests.Core;
-using System.Net;
-using JetBrains.Annotations;
 
 namespace Krosoft.Extensions.Samples.DotNet9.Api.Tests.Functional;
 
@@ -22,5 +23,32 @@ public class JobsEndpointTests : SampleBaseApiTest<Program>
         Check.That(jobs).IsNotNull();
         Check.That(jobs).HasSize(1);
         Check.That(jobs.Select(x => x.Identifiant)).ContainsExactly("CHECK");
+    }
+
+    [TestMethod]
+    public async Task Run_Ko()
+    {
+        var httpClient = Factory.CreateClient();
+        var response = await httpClient.PostAsync("/Jobs/Run/Test", null);
+
+        Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.InternalServerError);
+
+        var errorDto = await response.Content.ReadAsNewtonsoftJsonAsync<ErrorDto>(CancellationToken.None);
+        Check.That(errorDto).IsNotNull();
+        Check.That(errorDto!.Code).IsEqualTo(500);
+        Check.That(errorDto.Errors).HasSize(1);
+        Check.That(errorDto.Errors).ContainsExactly("Job 'Test' introuvable.");
+    }
+
+    [TestMethod]
+    public async Task Run_Ok()
+    {
+        var httpClient = Factory.CreateClient();
+        var response = await httpClient.PostAsync("/Jobs/Run/CHECK", null);
+
+        Check.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
+
+        var content = await response.Content.ReadAsStringAsync();
+        Check.That(content).IsEmpty();
     }
 }
