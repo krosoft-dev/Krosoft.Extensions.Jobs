0fbf0c5e1eff49a25fd30f0679e20734168ff15c
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-04-09 15:53:33 +0200

    maj

--- tests/Krosoft.Extensions.Jobs.Hangfire.Tests/Extensions/JobExtensionsTests.cs
+++ tests/Krosoft.Extensions.Jobs.Hangfire.Tests/Extensions/JobExtensionsTests.cs
@@ -1,6 +1,7 @@
 using Hangfire;
 using Hangfire.Common;
 using Krosoft.Extensions.Jobs.Hangfire.Extensions;
+using Krosoft.Extensions.Jobs.Hangfire.Models;
 
 namespace Krosoft.Extensions.Jobs.Hangfire.Tests.Extensions;
 
@@ -10,29 +11,29 @@ public class JobExtensionsTests
     [TestMethod]
     public void GetFingerprintKey_WithTypeAndMethod_ReturnsExpectedKey()
     {
-        var method = typeof(SampleJobService).GetMethod(nameof(SampleJobService.Execute))!;
-        var job = new Job(typeof(SampleJobService), method, "arg1", "arg2");
+        var method = typeof(SampleJob).GetMethod(nameof(SampleJob.ExecuteAsync))!;
+        var job = new Job(typeof(SampleJob), method, "arg1");
 
         var result = job.GetFingerprintKey();
 
-        Check.That(result).IsEqualTo($"fingerprint:{typeof(SampleJobService).FullName}.Execute.arg1.arg2");
+        Check.That(result).IsEqualTo($"fingerprint:{typeof(SampleJob).FullName}.ExecuteAsync.arg1");
     }
 
     [TestMethod]
     public void GetFingerprintKey_WithSingleArg_ReturnsExpectedKey()
     {
-        var method = typeof(SampleJobService).GetMethod(nameof(SampleJobService.Run))!;
-        var job = new Job(typeof(SampleJobService), method, "myArg");
+        var method = typeof(SampleJob).GetMethod(nameof(SampleJob.ExecuteAsync))!;
+        var job = new Job(typeof(SampleJob), method, "myArg");
 
         var result = job.GetFingerprintKey();
 
-        Check.That(result).IsEqualTo($"fingerprint:{typeof(SampleJobService).FullName}.Run.myArg");
+        Check.That(result).IsEqualTo($"fingerprint:{typeof(SampleJob).FullName}.ExecuteAsync.myArg");
     }
 
     [TestMethod]
     public void GetFingerprintKey_DynamicJob_ResolvesRealType()
     {
-        var dynamicJob = CreateDynamicJob(typeof(SampleJobService).AssemblyQualifiedName!,
+        var dynamicJob = CreateDynamicJob(typeof(SampleJob).AssemblyQualifiedName!,
                                           "Execute",
                                           "[\"\\\"value1\\\"\"]");
 
@@ -42,7 +43,7 @@ public class JobExtensionsTests
         var result = job.GetFingerprintKey();
 
         Check.That(result).StartsWith("fingerprint:");
-        Check.That(result).Contains(typeof(SampleJobService).FullName!);
+        Check.That(result).Contains(typeof(SampleJob).FullName!);
         Check.That(result).Contains("Execute");
         Check.That(result).Contains("value1");
     }
@@ -50,7 +51,7 @@ public class JobExtensionsTests
     [TestMethod]
     public void GetFingerprintKey_DynamicJob_NullArgs_ReturnsKeyWithEmptyArgs()
     {
-        var dynamicJob = CreateDynamicJob(typeof(SampleJobService).AssemblyQualifiedName!,
+        var dynamicJob = CreateDynamicJob(typeof(SampleJob).AssemblyQualifiedName!,
                                           "Execute",
                                           null!);
 
@@ -66,7 +67,7 @@ public class JobExtensionsTests
     [TestMethod]
     public void GetFingerprintKey_DynamicJob_EmptyArgs_ReturnsKeyWithEmptyArgs()
     {
-        var dynamicJob = CreateDynamicJob(typeof(SampleJobService).AssemblyQualifiedName!,
+        var dynamicJob = CreateDynamicJob(typeof(SampleJob).AssemblyQualifiedName!,
                                           "Execute",
                                           "");
 
@@ -80,14 +81,9 @@ public class JobExtensionsTests
 
     private static DynamicJob CreateDynamicJob(string type, string method, string args) => new(type, method, "", args, Array.Empty<JobFilterAttribute>(), "");
 
-    public class SampleJobService
+    internal class SampleJob : IRecurringJob
     {
-        public void Execute(string arg1, string arg2)
-        {
-        }
-
-        public void Run(string arg)
-        {
-        }
+        public string Type => nameof(SampleJob);
+        public Task<JobResult> ExecuteAsync(string identifiant) => throw new NotImplementedException();
     }
 }
